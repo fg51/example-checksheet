3a4a58d73fb3463559d3632c63c526569d4b0882
kflange <zknsftk1msrk@gmail.com>
2020-05-29 09:23:18 +0900

    Added: mockup

---
+++ client/src/components/app-footer/app-footer.tsx
@@ -0,0 +1,16 @@
+import { Component, ComponentInterface, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-footer',
+  styleUrl: 'app-footer.css',
+  shadow: true,
+})
+export class AppFooter implements ComponentInterface {
+  render() {
+    return (
+      <ion-footer>
+        ver.0.0.1-develop 作成日: 2020-05-29 作成者: Alice 承認者: Bob
+      </ion-footer>
+    );
+  }
+}
